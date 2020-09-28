744c9236e99cccc95a75027e009cb3626bdfa42a
Pritpal Singh Banga <pbanga@amazon.com>
2020-09-28 08:30:08 -0700

    Adding continue, break and pass flow controls

---
+++ FlowControl/ContinueBreak.py
@@ -0,0 +1,14 @@
+for val in 'String':
+    if val == 'i':
+        break
+    print(val)
+print('The end')
+
+print()
+
+for val in 'String':
+    if val == 'i':
+        continue
+    print(val)
+print('The end')
+
---
+++ FlowControl/Pass.py
@@ -0,0 +1,13 @@
+'''pass is just a placeholder for
+functionality to be added later.'''
+
+
+for val in 'String':
+    pass
+
+
+class Hello:
+    pass
+
+def function(args):
+    pass
